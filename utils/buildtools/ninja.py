3127fb2dd8f01ca69479ca43bc369456ae92ce11
Leedehai <leedehai@outlook.com>
2019-07-09 20:45:16 -0700

    Add 's' prefix to ninja status time value

--- utils/buildtools/ninja.py
+++ utils/buildtools/ninja.py
@@ -25,7 +25,7 @@ BIN_PATH = os.path.join(BIN_DIR, "ninja")
 # %f: finished_tasks_count %t: total_tasks_count
 # %e: elapsed_time (seconds in floating number)
 ENVIRON = dict(os.environ)
-ENVIRON.update({ "NINJA_STATUS": "[%f/%t:%e] "})
+ENVIRON.update({ "NINJA_STATUS": "[%f/%t:%es] "})
 def execute(program, args):
     # Popen.wait() returns the child's return code
     return subprocess.Popen([ program ] + args, env=ENVIRON).wait()
